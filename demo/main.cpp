8413cb1fdc9762d2e2e3b433c14f395e76fbb905
ken01235 <kenkenrc08@gmail.com>
2021-03-21 06:16:37 -0700

    modified:   ../5_2_textLCD/main.cpp 	modified:   ../5_3_uLCD/main.cpp 	modified:   ../5_4_uLCD_demo/main.cpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,20 +1,32 @@
 #include "mbed.h"
 #include "uLCD_4DGL.h"
+#include "TextLCD.h"
 
-uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+// uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+// 
+// int main()
+// {
+//     uLCD.cls();
+//     uLCD.background_color(WHITE);
+//     uLCD.color(BLUE);
+//     uLCD.printf("\n108061207\n");
+//     uLCD.text_width(4); //4X size text
+//     uLCD.text_height(4);
+//     uLCD.color(GREEN);
+//     for (int i = 30; i >= 0; --i) {
+//         uLCD.locate(1,2);
+//         uLCD.printf("%2d",i);
+//         ThisThread::sleep_for(1s);
+//     }
+// }
+
+TextLCD lcd(D2, D3, D4, D5, D6, D7); // RS, E, DB4-DB7
 
 int main()
 {
-    uLCD.cls();
-    uLCD.background_color(WHITE);
-    uLCD.color(BLUE);
-    uLCD.printf("\n108061207\n");
-    uLCD.text_width(4); //4X size text
-    uLCD.text_height(4);
-    uLCD.color(GREEN);
-    for (int i = 30; i >= 0; --i) {
-        uLCD.locate(1,2);
-        uLCD.printf("%2d",i);
+    for (int x = 30; x >= 0; x--) {
+        lcd.cls();
+        lcd.printf("108061207\n       %2d", x);
         ThisThread::sleep_for(1s);
     }
 }
